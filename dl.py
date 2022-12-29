c441dd4ded2ae9fcdb03a52c02a8149f1ddccacc
pullquery <pullquery@gmail.com>
2022-12-29 10:02:23 +0900

    hug

--- dl.py
+++ dl.py
@@ -1,7 +1,7 @@
 import os
 
 urls = {
-    "painful": "https://youtu.be/2kVl78-Jcoc"
+    "hug": "https://youtu.be/WMG-4DlRyqg"
 }
 
 for i in urls.items():
@@ -9,5 +9,5 @@ for i in urls.items():
     print(f"{title}: {url}")
 
     raw_url = os.popen(f"youtube-dl -g -x {url}").read().removesuffix("\n")
-    comm = f"ffmpeg -ss 00:02:56.00 -i \"{raw_url}\" -t 00:05:20.00 -c mp3 dl/{title}.mp3"
+    comm = f"ffmpeg -ss 00:07:32.00 -i \"{raw_url}\" -t 00:10:35.00 -c mp3 dl/{title}.mp3"
     os.system(comm)
